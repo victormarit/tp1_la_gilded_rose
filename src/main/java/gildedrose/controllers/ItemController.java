b7d381fa9f0b6ff7dc8789ac9ea3ff5dec4054c2
greggameplayer <gregoire.hage@gmail.com>
2022-11-02 13:21:53 +0100

    enhance

--- src/main/java/gildedrose/controllers/ItemController.java
+++ src/main/java/gildedrose/controllers/ItemController.java
@@ -6,7 +6,10 @@ import gildedrose.enums.ItemStates;
 
 public class ItemController {
 
-    public static void qualityIsNotNegative(Item item){
+    private ItemController() {
+    }
+
+    public static void qualityIsNotNegative(Item item) {
         if (item.getQuality() >= 0) {
             ItemStates.QualityIsNotNegative.nextState().updateQuality(item);
         }
@@ -14,20 +17,18 @@ public class ItemController {
 
     public static void itemIsNotAgedBrieOrBackstagePasses(Item item) {
         if (!item.getName().equals(ItemName.AGED_BRIE.getName())
-            && !item.getName().equals(ItemName.BACKSTAGE_PASSES.getName())) {
-            if (!item.getName().equals(ItemName.SULFURAS.getName())) {
-                if (item.getSellIn() <= 0) {
-                    item.setQuality(item.getQuality() - (item.getName().equals(ItemName.CONJURED.getName()) ? 4 : 2));
-                } else {
-                    item.setQuality(item.getQuality() - (item.getName().equals(ItemName.CONJURED.getName()) ? 2 : 1));
-                }
+            && !item.getName().equals(ItemName.BACKSTAGE_PASSES.getName()) && !item.getName().equals(ItemName.SULFURAS.getName())) {
+            if (item.getSellIn() <= 0) {
+                item.setQuality(item.getQuality() - (item.getName().equals(ItemName.CONJURED.getName()) ? 4 : 2));
+            } else {
+                item.setQuality(item.getQuality() - (item.getName().equals(ItemName.CONJURED.getName()) ? 2 : 1));
             }
         }
         ItemStates.ItemIsNotAgedBrieOrBackstagePasses.nextState().updateQuality(item);
     }
 
 
-    public static void qualityIsLessThan51AndItemIsAgedBrieOrBackstagePasses(Item item){
+    public static void qualityIsLessThan51AndItemIsAgedBrieOrBackstagePasses(Item item) {
         if (item.getName().equals(ItemName.AGED_BRIE.getName()) || item.getName().equals(ItemName.BACKSTAGE_PASSES.getName())) {
             if (item.getSellIn() < 6 && (item.getQuality() + 3) < 51) {
                 item.setQuality(Math.min(item.getQuality() + 3, 50));
